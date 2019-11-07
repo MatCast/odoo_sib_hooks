a4903e3c48aafead75840139ce7a5b1a996e5a3c
MatCast <gozyssj4@gmail.com>
2019-11-07 11:55:35 +0100

    Added class to control how the data is handeld: data_handler.py

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,26 +1,15 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 import logging
-import data_handler as dh
+from . import data_handler as dh
 _logger = logging.getLogger(__name__)
 
 
-
 class ToPost(http.Controller):
-    @http.route('/to_post/to_post/', auth='public')
-    def index(self, **kw):
-        return "Hello, world"
-
-    @http.route('/to_post/post_here/', type='json',
+    @http.route('/to_post/sib_contact_update/', type='json',
                 website=True, auth="public",
                 methods=['POST'], csrf=False)
     def insert_library_book(self, **kwargs):
-        # record = request.env['library.book']
-        # record.create(kwargs)
-        data = http.request.httprequest.data
-        headers = http.request.httprequest.headers
-        data_in_json = yaml.load(data)
-        records = http.request.env['crm.lead'].sudo().search([])
-        records[0].write({'source_id': 'Facebook'})
-        _logger.info('HERE LOGGING{0}!!!!!!!!!!!!'.format(records))
-        return 'Hey There ###: <br>{0}'.format(records[0].name)
+        client = dh.Client(http.request.httprequest)
+        client.update_lead()
+        return 'Hey There ###: {0}'.format('ciao')
--- controllers/data_handler.py
+++ controllers/data_handler.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 import yaml
 import logging
+from odoo.http import request
 
 _logger = logging.getLogger(__name__)
 
 SIB_MAP_TXT = {
-    'email': 'partner_address_email',
+    'email': 'email_from',
     'sms': 'phone',
     'azienda': 'partner_name',
     'nome_e_cognome': 'street',
@@ -16,14 +17,19 @@ SIB_MAP_TXT = {
     }
 
 SIB_MAP_DB = {
-    'provincia': {'res.country.state': 'state_id'},
-    'utm_source': {'utm.source': 'source_id'},
-    'utm_campaign': {'utm.campaign': 'campaign_id'},
-    'utm_medium': {'utm.medium': 'medium_id'},
+    'provincia': 'res.country.state',
+    'utm_source': 'utm.source',
+    'utm_campaign': 'utm.campaign',
+    'utm_medium': 'utm.medium',
+}
+
+MODEL_ID = {
+    'res.country.state': 'state_id',
+    'utm.source': 'source_id',
+    'utm.campaign': 'campaign_id',
+    'utm.medium': 'medium_id',
 }
 """
-COGNOME
-NOME
 DOUBLE_OPT-IN
 PRIVACY
 UTM_TERM
@@ -37,8 +43,10 @@ EMAIL_COUNT
 class Client(object):
     def __init__(self, response):
         self.response = response
-        self.data = self._parse_data()
+        self.data = self._data_attributes()
         self.headers = response.headers
+        self._map_data()
+        self._parse_name()
 
     def _parse_data(self):
         try:
@@ -48,5 +56,74 @@ class Client(object):
             data_json = {}
         return data_json
 
+    def _data_attributes(self):
+        data_json = self._parse_data()
+        attr = data_json['content'][0]['attributes']
+        return dict((k.lower(), v) for k, v in attr.items())
+
     def _map_data(self):
-        if self.data:
+        # looks like {'partner_address_email' : 'example@email.com'}
+        self.txt_map = {}
+        # looks like {'res.country.state' : 'Vercelli'}
+        self.db_map = {}
+        # looks like self.data = {'email': 'example@email.com'}
+        for key in self.data:
+            if key in SIB_MAP_TXT.keys():
+                # looks like SIB_MAP_TXT = {'email': 'partner_address_email'}
+                self.txt_map[SIB_MAP_TXT[key]] = self.data[key]
+            elif key in SIB_MAP_DB.keys():
+                # looks like SIB_MAP_DB = {'provincia': 'res.country.state'}
+                self.db_map[SIB_MAP_DB[key]] = self.data[key]
+
+    def _parse_name(self):
+        """Put together name and surname."""
+        k = self.data.keys()
+        if 'cognome' in k:
+            surname = self.data['cognome']
+        if 'nome' in k:
+            name = surname = self.data['nome']
+        if name or surname:
+            self.txt_map['contact_name'] = (name + ' '
+                                            + surname)
+            self.txt_map['contact_name'] = self.txt_map['contact_name'].strip()
+
+    def _find_record(self):
+        if 'id' in self.txt_map.keys():
+            lead_id = self.txt_map['id']
+            records = request.env['crm.lead'].sudo().search([('id',
+                                                              '=',
+                                                              lead_id)])
+            if records:
+                return records[0]
+
+    def _handle_txt(self):
+        record = self._find_record()
+        # _logger.info('HERE LOGGING pizza1 {0}!!!!!!!!!!!!'.format(record.email_from))
+        if record:
+            map_copy = self.txt_map.copy()
+            map_copy.pop('id')
+            record.write(map_copy)
+
+    def _serach_db(self):
+        # looks like {'state_id' : 23}
+        to_write = {}
+        for key in self.db_map:
+            records = request.env[key].sudo().search([('name',
+                                                       '=ilike',
+                                                       self.db_map[key])])
+            if len(records) == 1:
+                to_write[MODEL_ID[key]] = records[0].id
+            if not records:
+                rec = request.env[key].create({'name': self.db_map[key]})
+                to_write[MODEL_ID[key]] = rec.id
+        return to_write
+
+    def _handle_db(self):
+        record = self._find_record()
+        to_write = self._serach_db()
+        if record:
+            record.write(to_write)
+
+    def update_lead(self):
+        self._handle_txt()
+        self._handle_db()
