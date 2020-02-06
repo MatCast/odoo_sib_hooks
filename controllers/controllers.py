2945353e8044e886f2a6b0e54202affca47e34fa
MatCast <gozyssj4@gmail.com>
2020-02-06 12:12:49 +0100

    Added logging Info

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -10,13 +10,19 @@ class ToPost(http.Controller):
                 website=True, auth="public",
                 methods=['GET', 'POST'], csrf=False)
     def insert_library_book(self, **kwargs):
+        _logger.info('### SIB POSTING START ###')
+        string_200 = 'SIB Lead {0} Updated. ID: {1}'
         client = dh.Client(http.request.httprequest)
         if client.user_agent:
             client.update_lead()
             result = 'No lead Updated'
             if client.record:
-                result = 'Lead {0} Updated. ID: {1}'.format(client.record.name,
-                                                            client.record.id)
+                result = string_200.format(client.record.name,
+                                           client.record.id)
+                _logger.info(string_200.format(client.record.name,
+                                               client.record.id))
         else:
             result = 'Invalid User Agent'
+            _logger.info('SIB Invalid User Agent')
+        _logger.info('### SIB POSTING END ###')
         return result
